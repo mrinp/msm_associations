70b766f6bf00bb5e14c60a51c5545988826c9309
mrinp <mrinp@users.noreply.github.com>
2017-05-19 13:26:17 -0500

    End of Class Changes

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,6 +1,6 @@
-class Director < ApplicationRecord
+class Actor < ApplicationRecord
 
-  # Director:
+  # Actor:
   #  - name: must be present; must be unique in combination with dob
   validates :name, :presence => true, :uniqueness => {:scope => :dob}
 
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -8,16 +8,17 @@ class Movie < ApplicationRecord
   validates :title, :presence => true, :uniqueness => {:scope => :year}
 
   #  - year: must be integer between 1870 and 2050
-
+  validates :year, :numericality => {:only_integer => true, :greater_than_or_equal_to => 1870, :less_than_or_equal_to => 2050}
 
   #  - duration: must be integer between 0 and 2764800, but it can be blank (hint: there is an option `:allow_blank => true`)
-
+  validates :duration, :numericality => {:only_integer => true, :greater_than_or_equal_to => 0, :less_than_or_equal_to => 2764800}
 
   #  - description: no rules
-
-
   #  - image_url: no rules
 
+  belongs_to :director, :class_name => "Director", :foreign_key => "director_id"
 
+  has_many :characters
+  has_many :actors, :through => :characters
 
 end
